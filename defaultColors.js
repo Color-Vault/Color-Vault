a56c6ad0e4ac629f233697f05a5e8cc5e59c5f7b
masterwebx <59427887+masterwebx@users.noreply.github.com>
2024-06-29 15:08:48 -0700

    Update defaultColors.js

--- defaultColors.js
+++ defaultColors.js
@@ -3,235 +3,6 @@ const defaultColorGroups = [
 ];
 defaultColorGroups.push({
     "imageName": "Bandana Dee Recolor Sheet",
-   "groupName": "testlol222",
-    "groups": [
-        {
-            "name": "Face",
-            "colors": [
-                "rgb(241, 205, 138)",
-                "rgb(255, 233, 194)",
-                "rgb(220, 167, 102)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Bandana",
-            "colors": [
-                "rgb(57, 83, 240)",
-                "rgb(41, 64, 205)",
-                "rgb(32, 53, 177)",
-                "rgb(22, 38, 138)",
-                "rgb(14, 27, 107)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Body",
-            "colors": [
-                "rgb(237, 78, 43)",
-                "rgb(255, 110, 81)",
-                "rgb(190, 53, 22)",
-                "rgb(147, 38, 14)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        }
-    ]
-});
-defaultColorGroups.push({
-    "imageName": "Bandana Dee Recolor Sheet",
-    "groupName": "testlol",
-    "groups": [
-        {
-            "name": "Face",
-            "colors": [
-                "rgb(241, 205, 138)",
-                "rgb(255, 233, 194)",
-                "rgb(220, 167, 102)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Bandana",
-            "colors": [
-                "rgb(57, 83, 240)",
-                "rgb(41, 64, 205)",
-                "rgb(32, 53, 177)",
-                "rgb(22, 38, 138)",
-                "rgb(14, 27, 107)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Body",
-            "colors": [
-                "rgb(237, 78, 43)",
-                "rgb(255, 110, 81)",
-                "rgb(190, 53, 22)",
-                "rgb(147, 38, 14)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        }
-    ]
-});
-defaultColorGroups.push({
-    "imageName": "Bandana Dee Recolor Sheet",
-    "groupName": "Has the BG baby",
-    "groups": [
-        {
-            "name": "Face",
-            "colors": [
-                "rgb(241, 205, 138)",
-                "rgb(255, 233, 194)",
-                "rgb(220, 167, 102)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Bandana",
-            "colors": [
-                "rgb(57, 83, 240)",
-                "rgb(41, 64, 205)",
-                "rgb(32, 53, 177)",
-                "rgb(22, 38, 138)",
-                "rgb(14, 27, 107)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Body",
-            "colors": [
-                "rgb(237, 78, 43)",
-                "rgb(255, 110, 81)",
-                "rgb(190, 53, 22)",
-                "rgb(147, 38, 14)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "BG color",
-            "colors": [
-                "rgb(63, 67, 90)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ba1c1c",
-            "tintStrength": 85
-        }
-    ]
-});
-defaultColorGroups.push({
-    "imageName": "Bandana Dee Recolor Sheet",
-    "groupName": "testing",
-    "authorName": "Creator3",
-    "groups": [
-        {
-            "name": "Face",
-            "colors": [
-                "rgb(241, 205, 138)",
-                "rgb(255, 233, 194)",
-                "rgb(220, 167, 102)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Bandana",
-            "colors": [
-                "rgb(57, 83, 240)",
-                "rgb(41, 64, 205)",
-                "rgb(32, 53, 177)",
-                "rgb(22, 38, 138)",
-                "rgb(14, 27, 107)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Body",
-            "colors": [
-                "rgb(237, 78, 43)",
-                "rgb(255, 110, 81)",
-                "rgb(190, 53, 22)",
-                "rgb(147, 38, 14)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        },
-        {
-            "name": "Group 4",
-            "colors": [
-                "rgb(23, 189, 25)"
-            ],
-            "hue": 0,
-            "brightness": 0,
-            "contrast": 0,
-            "saturation": 0,
-            "tintColor": "#ffffff",
-            "tintStrength": 0
-        }
-    ]
-});
-defaultColorGroups.push({
-    "imageName": "Bandana Dee Recolor Sheet",
     "groupName": "Funny Epic",
     "authorName": "Creator Wex",
     "groups": [
@@ -294,4 +65,70 @@ defaultColorGroups.push({
         }
     ]
 });
+defaultColorGroups.push({
+    "imageName": "Sandbag Recolor Sheet",
+    "groupName": "Sandbag Separation",
+    "authorName": "Creator Wex",
+    "groups": [
+        {
+            "name": "Front of the bag",
+            "colors": [
+                "rgb(240, 240, 255)",
+                "rgb(214, 212, 226)",
+                "rgb(185, 184, 195)",
+                "rgb(151, 149, 160)"
+            ],
+            "hue": 0,
+            "brightness": 0,
+            "contrast": -7,
+            "saturation": 0,
+            "tintColor": "#c510ea",
+            "tintStrength": 82
+        },
+        {
+            "name": "Back of the bag",
+            "colors": [
+                "rgb(110, 108, 116)",
+                "rgb(151, 149, 160)"
+            ],
+            "hue": 0,
+            "brightness": 0,
+            "contrast": 0,
+            "saturation": 0,
+            "tintColor": "#2bdc1e",
+            "tintStrength": 62
+        },
+        {
+            "name": "The golden beholden eye of the bag",
+            "colors": [
+                "rgb(244, 173, 9)",
+                "rgb(252, 209, 120)",
+                "rgb(167, 99, 7)",
+                "rgb(214, 126, 10)"
+            ],
+            "hue": 52,
+            "brightness": 0,
+            "contrast": 0,
+            "saturation": 0,
+            "tintColor": "#ffffff",
+            "tintStrength": 0
+        },
+        {
+            "name": "The eyes of the bag",
+            "colors": [
+                "rgb(41, 41, 43)",
+                "rgb(95, 95, 100)",
+                "rgb(49, 51, 57)",
+                "rgb(10, 10, 11)"
+            ],
+            "hue": 180,
+            "brightness": 100,
+            "contrast": 100,
+            "saturation": 100,
+            "tintColor": "#008fb3",
+            "tintStrength": 100
+        }
+    ]
+});
+
 
